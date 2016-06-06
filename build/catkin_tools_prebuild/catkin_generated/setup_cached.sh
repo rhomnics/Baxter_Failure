484659a741d3522fede5ccc4298a231ba3e5c6ac
Rhomni St. John <rhomnics@gmail.com>
2016-06-06 11:03:10 -0400

    now using catkin build

---
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rhomni/bax_ws/bax_local/devel/.private/catkin_tools_prebuild:/home/rhomni/bax_ws/bax_common/devel:/home/rhomni/bax_ws/bax_core/devel:/opt/ros/indigo:/home/rhomni/bax_ws/bax_local/devel"
+export LD_LIBRARY_PATH="/home/rhomni/bax_ws/bax_common/devel/lib:/home/rhomni/bax_ws/bax_core/devel/lib:/opt/ros/indigo/lib:/home/rhomni/bax_ws/bax_local/devel/lib"
+export PWD="/home/rhomni/bax_ws/bax_local/build/catkin_tools_prebuild"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rhomni/bax_ws/bax_local/devel/.private/catkin_tools_prebuild/share/common-lisp:/home/rhomni/bax_ws/bax_common/devel/share/common-lisp:/home/rhomni/bax_ws/bax_core/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/rhomni/bax_ws/bax_local/build/catkin_tools_prebuild:/home/rhomni/bax_ws/bax_common/src/baxter_head_toolkit:/home/rhomni/bax_ws/bax_core/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/rhomni/bax_ws/bax_local/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
